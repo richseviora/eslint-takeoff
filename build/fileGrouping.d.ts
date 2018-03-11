b4078afc435881b611ab9a21fad2bcac9255d532
Rich Seviora <richard.seviora@gmail.com>
2018-03-11 10:21:24 -0700

    Remove build from output

--- build/fileGrouping.d.ts
+++
@@ -1,6 +0,0 @@
-import { LintReport } from "eslint";
-import { FileAggregated, OverrideOutput } from "./models";
-export declare function reportToFileGroup(report: LintReport): FileAggregated[];
-export declare function fileGroupsToOverride(
-  aggregate: FileAggregated[],
-): OverrideOutput[];
--- build/ruleGrouping.d.ts
+++
@@ -1,6 +0,0 @@
-import { LintReport } from "eslint";
-import { RuleAggregated, OverrideOutput } from "./models";
-export declare function reportToRuleGroups(report: LintReport): RuleAggregated[];
-export declare function ruleGroupsToOverride(
-  aggregate: RuleAggregated[],
-): OverrideOutput[];
